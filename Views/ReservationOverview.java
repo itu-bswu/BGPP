438106a2bbf20fa09b5914d3d1b3ac5471a78e77
Ulrik Damm <ulrikdamm@me.com>
2010-12-02 00:21:16 +0100

    Opdatering til tabellen, tilføjet kommentarer

--- Views/ReservationOverview.java
+++ Views/ReservationOverview.java
@@ -5,9 +5,40 @@ package Views;
  *
  */
 public class ReservationOverview extends javax.swing.JFrame {
+	/**
+	 * CellState enum
+	 * Specifices 4 constants for marking a cell as the start, middle or end of a period
+	 * Used when drawing the cells
+	 */
+	private enum CellState {
+		CELL_START, CELL_MIDDLE, CELL_END, CELL_NONE
+	}
+	
+	CellState[][] carsStates;
+	
+	/**
+	 * ReservationOverview contructor
+	 */
 	public ReservationOverview() {
 		this.setSize(800, 600);
 		
+		//TODO: Load number of cars from database
+		carsStates = new CellState[15][7];
+		
+		//inserting dummy data
+		//TODO: Load data from database
+		for (CellState[] state : carsStates) {
+			state[0] = CellState.CELL_START;
+			state[1] = CellState.CELL_MIDDLE;
+			state[2] = CellState.CELL_MIDDLE;
+			state[3] = CellState.CELL_END;
+			state[4] = CellState.CELL_NONE;
+			state[5] = CellState.CELL_START;
+			state[6] = CellState.CELL_END;
+		}
+		
+		//date colums
+		//TODO: generate from date
 		String[] colums = {
 			"Car",
 			"1/12",
@@ -19,54 +50,27 @@ public class ReservationOverview extends javax.swing.JFrame {
 			"7/12"
 		};
 		
+		//cell contents
+		//TODO: Load cars from database
 		Object[][] data = {
-			{
-				"Lastbil 1", "", "", "", "", "", "", ""
-			},
-			{
-				"Lastbil 2", "", "", "", "", "", "", ""
-			},
-			{
-				"Lastbil 3", "", "", "", "", "", "", ""
-			},
-			{
-				"Sportsvogn 1", "", "", "", "", "", "", ""
-			},
-			{
-				"Sportsvogn 2", "", "", "", "", "", "", ""
-			},
-			{
-				"Sportsvogn 2", "", "", "", "", "", "", ""
-			},
-			{
-				"Sportsvogn 3", "", "", "", "", "", "", ""
-			},
-			{
-				"Sportsvogn 4", "", "", "", "", "", "", ""
-			},
-			{
-				"Sportsvogn 5", "", "", "", "", "", "", ""
-			},
-			{
-				"Varevogn 1", "", "", "", "", "", "", ""
-			},
-			{
-				"Varevogn 2", "", "", "", "", "", "", ""
-			},
-			{
-				"Varevogn 3", "", "", "", "", "", "", ""
-			},
-			{
-				"hundesl\u00E6de 1", "", "", "", "", "", "", ""
-			},
-			{
-				"hundesl\u00E6de 2", "", "", "", "", "", "", ""
-			},
-			{
-				"Din mor", "", "", "", "", "", "", ""
-			}
+			{ "Lastbil 1", "", "", "", "", "", "", "" },
+			{ "Lastbil 2", "", "", "", "", "", "", "" },
+			{ "Lastbil 3", "", "", "", "", "", "", "" },
+			{ "Sportsvogn 1", "", "", "", "", "", "", "" },
+			{ "Sportsvogn 2", "", "", "", "", "", "", "" },
+			{ "Sportsvogn 2", "", "", "", "", "", "", "" },
+			{ "Sportsvogn 3", "", "", "", "", "", "", "" },
+			{ "Sportsvogn 4", "", "", "", "", "", "", "" },
+			{ "Sportsvogn 5", "", "", "", "", "", "", "" },
+			{ "Varevogn 1", "", "", "", "", "", "", "" },
+			{ "Varevogn 2", "", "", "", "", "", "", "" },
+			{ "Varevogn 3", "", "", "", "", "", "", "" },
+			{ "hundesl\u00E6de 1", "", "", "", "", "", "", "" },
+			{ "hundesl\u00E6de 2", "", "", "", "", "", "", "" },
+			{ "Din mor", "", "", "", "", "", "", "" }
 		};
 		
+		//creates the table
 		javax.swing.JTable table = new javax.swing.JTable(data, colums);
 		table.setShowGrid(true);
 		table.setShowHorizontalLines(true);
@@ -89,13 +93,56 @@ public class ReservationOverview extends javax.swing.JFrame {
 		this.setVisible(true);
 	}
 	
-	public class TableCellRenderer extends javax.swing.table.DefaultTableCellRenderer {
-		public java.awt.Component getTableCellRendererComponent(javax.swing.JTable table, Object value, boolean selected, boolean focus, int row, int colum) {
-			java.awt.Component cell = super.getTableCellRendererComponent(table, value, selected, focus, row, colum);
+	/**
+	 * class TableCellRenderer
+	 * custom renderer class for the cells in the table
+	 * extends JPanel instead of the default JLabel, since no text is needed
+	 */
+	public class TableCellRenderer extends javax.swing.JPanel implements javax.swing.table.TableCellRenderer {
+		private int row;
+		private int column;
+		
+		/**
+		 * getTableCellRendererComponent
+		 * method defined in javax.swing.table.TableCellRenderer
+		 * @param table the table
+		 * @param value ??
+		 * @param selected wether the cell is selected or not
+		 * @param focus wether the cell is in focus or not
+		 * @param row the cell's row index
+		 * @param column the cell's column index
+		 */
+		public java.awt.Component getTableCellRendererComponent(javax.swing.JTable table, Object value, boolean selected, boolean focus, int row, int column) {
 			
-			cell.setBackground(java.awt.Color.BLUE);
+			this.row = row;
+			this.column = column;
 			
-			return cell;
+			return this;
+		}
+		
+		/**
+		 * paintComponent
+		 * draws the cell contents
+		 * @param g the java.awt.Graphics object
+		 */
+		@Override public void paintComponent(java.awt.Graphics g) {
+			super.paintComponent(g);
+			
+			g.setColor(java.awt.Color.BLUE);
+			
+			switch (carsStates[row][column-1]) {
+				case CELL_START:
+					g.fillRect(5, 5, this.getWidth()-5, this.getHeight()-10);
+					break;
+				case CELL_MIDDLE:
+					g.fillRect(0, 5, this.getWidth(), this.getHeight()-10);
+					break;
+				case CELL_END:
+					g.fillRect(0, 5, this.getWidth()-5, this.getHeight()-10);
+					break;
+				default:
+					break;
+			}
 		}
 	}
 }
