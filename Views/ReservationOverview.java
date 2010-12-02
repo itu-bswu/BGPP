755ce7c64bb75cdc9bb85367ed202dee2cb32875
Ulrik Damm <ulrikdamm@me.com>
2010-12-02 14:41:23 +0100

    Bruger nu import i stedet for at specifiere package path hver gang

--- Views/ReservationOverview.java
+++ Views/ReservationOverview.java
@@ -1,10 +1,14 @@
 package Views;
 
+import javax.swing.*;
+import javax.swing.table.*;
+import java.awt.*;
+
 /**
  * View - Reservation Overview
  *
  */
-public class ReservationOverview extends javax.swing.JFrame {
+public class ReservationOverview extends JFrame {
 	/**
 	 * CellState enum
 	 * Specifices 4 constants for marking a cell as the start, middle or end of a period
@@ -22,27 +26,27 @@ public class ReservationOverview extends javax.swing.JFrame {
 	public ReservationOverview() {
 		this.setSize(800, 400);
 		
-		javax.swing.JMenuBar menuBar = new javax.swing.JMenuBar();
+		JMenuBar menuBar = new JMenuBar();
 		this.setJMenuBar(menuBar);
 		
-		javax.swing.JMenu fileMenu = new javax.swing.JMenu("File");
+		JMenu fileMenu = new JMenu("File");
 		menuBar.add(fileMenu);
 		
-		javax.swing.JMenuItem newReservationItem = new javax.swing.JMenuItem("New reservation...");
+		JMenuItem newReservationItem = new JMenuItem("New reservation...");
 		fileMenu.add(newReservationItem);
 		
 		fileMenu.addSeparator();
-		javax.swing.JMenuItem quitItem = new javax.swing.JMenuItem("Quit");
+		JMenuItem quitItem = new JMenuItem("Quit");
 		fileMenu.add(quitItem);
 		
-		javax.swing.JMenu viewMenu = new javax.swing.JMenu("View");
+		JMenu viewMenu = new JMenu("View");
 		menuBar.add(viewMenu);
 		
-		javax.swing.JMenuItem gotoItem = new javax.swing.JMenuItem("Go to date...");
+		JMenuItem gotoItem = new JMenuItem("Go to date...");
 		viewMenu.add(gotoItem);
 		
 		viewMenu.addSeparator();
-		javax.swing.JMenuItem customerItem = new javax.swing.JMenuItem("View customer list...");
+		JMenuItem customerItem = new JMenuItem("View customer list...");
 		viewMenu.add(customerItem);
 		
 		//TODO: Load number of cars from database
@@ -94,10 +98,10 @@ public class ReservationOverview extends javax.swing.JFrame {
 		};
 		
 		//creates the table
-		javax.swing.JTable table = new javax.swing.JTable(data, colums);
+		JTable table = new JTable(data, colums);
 		table.setShowGrid(true);
 		table.setShowHorizontalLines(true);
-		table.setGridColor(java.awt.Color.GRAY);
+		table.setGridColor(Color.GRAY);
 		table.setRowHeight(25);
 		
 		for (int i = 0; i < table.getColumnModel().getColumnCount(); i++) {
@@ -105,21 +109,21 @@ public class ReservationOverview extends javax.swing.JFrame {
 				table.getColumnModel().getColumn(i).setMinWidth(150);
 				table.getColumnModel().getColumn(i).setMaxWidth(150);
 			} else {
-				table.getColumnModel().getColumn(i).setCellRenderer(new TableCellRenderer());
+				table.getColumnModel().getColumn(i).setCellRenderer(new CustomTableCellRenderer());
 			}
 		}
 		
-		this.setLayout(new java.awt.BorderLayout());
-		this.add(table.getTableHeader(), java.awt.BorderLayout.NORTH);
-		this.add(new javax.swing.JScrollPane(table), java.awt.BorderLayout.CENTER);
+		this.setLayout(new BorderLayout());
+		this.add(table.getTableHeader(), BorderLayout.NORTH);
+		this.add(new JScrollPane(table), BorderLayout.CENTER);
 		
 		//bottom panel
-		javax.swing.JPanel bottomPanel = new javax.swing.JPanel();
-		bottomPanel.setLayout(new java.awt.BorderLayout());
-		bottomPanel.add(new javax.swing.JButton("<"), java.awt.BorderLayout.WEST);
-		bottomPanel.add(new javax.swing.JButton(">"), java.awt.BorderLayout.EAST);
-		bottomPanel.add(new javax.swing.JButton("Go to date..."), java.awt.BorderLayout.CENTER);
-		this.add(bottomPanel, java.awt.BorderLayout.SOUTH);
+		JPanel bottomPanel = new JPanel();
+		bottomPanel.setLayout(new BorderLayout());
+		bottomPanel.add(new JButton("<"), BorderLayout.WEST);
+		bottomPanel.add(new JButton(">"), BorderLayout.EAST);
+		bottomPanel.add(new JButton("Go to date..."), BorderLayout.CENTER);
+		this.add(bottomPanel, BorderLayout.SOUTH);
 		
 		this.setVisible(true);
 	}
@@ -129,13 +133,13 @@ public class ReservationOverview extends javax.swing.JFrame {
 	 * custom renderer class for the cells in the table
 	 * extends JPanel instead of the default JLabel, since no text is needed
 	 */
-	public class TableCellRenderer extends javax.swing.JPanel implements javax.swing.table.TableCellRenderer {
+	public class CustomTableCellRenderer extends JPanel implements TableCellRenderer {
 		private int row;
 		private int column;
 		
 		/**
 		 * getTableCellRendererComponent
-		 * method defined in javax.swing.table.TableCellRenderer
+		 * method defined in table.TableCellRenderer
 		 * @param table the table
 		 * @param value ??
 		 * @param selected wether the cell is selected or not
@@ -143,7 +147,7 @@ public class ReservationOverview extends javax.swing.JFrame {
 		 * @param row the cell's row index
 		 * @param column the cell's column index
 		 */
-		public java.awt.Component getTableCellRendererComponent(javax.swing.JTable table, Object value, boolean selected, boolean focus, int row, int column) {
+		public Component getTableCellRendererComponent(JTable table, Object value, boolean selected, boolean focus, int row, int column) {
 			
 			this.row = row;
 			this.column = column;
@@ -154,12 +158,12 @@ public class ReservationOverview extends javax.swing.JFrame {
 		/**
 		 * paintComponent
 		 * draws the cell contents
-		 * @param g the java.awt.Graphics object
+		 * @param g the Graphics object
 		 */
-		@Override public void paintComponent(java.awt.Graphics g) {
+		@Override public void paintComponent(Graphics g) {
 			super.paintComponent(g);
 			
-			g.setColor(java.awt.Color.BLUE);
+			g.setColor(Color.BLUE);
 			
 			switch (carsStates[row][column-1]) {
 				case CELL_START:
