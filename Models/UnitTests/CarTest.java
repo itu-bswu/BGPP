aba938f825d7977cb631504ec4166add3b243d7d
Niklas Hansen <nikl@itu.dk>
2010-12-02 16:05:13 +0100

    Finalized unit tests for model Car

--- Models/UnitTests/CarTest.java
+++ Models/UnitTests/CarTest.java
@@ -57,8 +57,49 @@ public class CarTest {
 		
 		int car1 = car.create(createVars);
 		
-		assertTrue(car1 > 0);
-		assertTrue(car.read(car1) != null);
-		assertTrue(car.delete(car1));
+		assertTrue(car1 > 0); // Test #1
+		assertTrue(car.read(car1) != null); // Test #2
+		assertTrue(car.delete(car1)); // Test #3
+	}
+	
+	/**
+	 * Test - Car #4-7
+	 */
+	@Test
+	public void testMultipleCars () {
+		Map<String, Object> createVars = new HashMap<String, Object>();
+		createVars.put("type", varevogn);
+		createVars.put("car", "Ford Transit");
+		createVars.put("licenseplate", "SV 32 654");
+		int car1 = car.create(createVars);
+		
+		createVars = new HashMap<String, Object>();
+		createVars.put("type", sportsvogn);
+		createVars.put("car", "Mazda MX-5");
+		createVars.put("licenseplate", "VS 12 345");
+		int car2 = car.create(createVars);
+		
+		assertTrue(car1 > 0); // Test #4
+		assertTrue(car2 > 0); // Test #4
+		assertTrue(car.delete(car2)); // Test #5
+		
+		car2 = car.create(createVars);
+		
+		assertTrue(car2 > 0); // Test #6
+		assertFalse(car.create(createVars) > 0); // Test #7
+	}
+	
+	/**
+	 * Test - Car #8
+	 */
+	@Test
+	public void testUnknownType () {
+		int submarine = 1337;
+		Map<String, Object> createVars = new HashMap<String, Object>();
+		createVars.put("type", submarine);
+		createVars.put("car", "Ford Transit");
+		createVars.put("licenseplate", "SV 32 654");
+		
+		assertFalse(car.create(createVars) > 0); // Test #8
 	}
 }
