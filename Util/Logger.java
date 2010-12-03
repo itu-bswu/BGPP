649bb70e1c3ce6c1a32f4858051949d6c152c06a
Niklas Hansen <nikl@itu.dk>
2010-12-03 13:32:03 +0100

    Added basic logging

---
+++ Util/Logger.java
@@ -0,0 +1,19 @@
+package Util;
+
+/**
+ * Logger.
+ * Class made for error logging.
+ * 
+ * <code>
+ *     Logger.write("This is a sample error-message");
+ * </code>
+ * 
+ * @author Niklas Hansen
+ * @version 0.1
+ */
+public class Logger {
+	
+	static public void write (String errorMsg) {
+		// Do some writing
+	}
+}
