446e18a5e60fccbc07ab4754a7de0c987c1cef0f
Niklas Hansen <nikl@itu.dk>
2010-12-03 14:58:11 +0100

    Implemented Logger class

--- Util/Logger.java
+++ Util/Logger.java
@@ -1,11 +1,27 @@
 package Util;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Logger.
  * Class made for error logging.
+ * It is possible to record a log message without creating an instance of 
+ * the class by calling the static method write().
  * 
  * <code>
  *     Logger.write("This is a sample error-message");
+ * </code>
+ * 
+ * Or you can create an instance of the class.
+ * 
+ * <code>
+ *     Logger log = new Logger();
+ *     log.writeToLog("This is a sample error-message");
+ *     log.close();
  * </code>
  * 
  * @author Niklas Hansen
@@ -13,7 +29,43 @@ package Util;
  */
 public class Logger {
 	
+	FileWriter writer;
+	
+	static private Logger logger;
+	
+	public Logger () {
+		try {
+			this.writer = new FileWriter("error.log");
+		} catch (IOException e) {}
+	}
+	
 	static public void write (String errorMsg) {
-		// Do some writing
+		logger = new Logger();
+		logger.writeToLog(errorMsg);
+		logger.close();
+		logger = null;
+	}
+	
+	public void writeToLog (String errorMsg) {
+		if (writer == null)
+			return;
+		
+		try {
+			DateFormat dateFormat = new SimpleDateFormat();
+			Date date = new Date();
+			
+			writer.write('[' + dateFormat.format(date) + ']');
+			writer.write(" " + errorMsg);
+			writer.write('\n');
+		} catch (IOException e) {}
+	}
+	
+	public void close () {
+		if (writer == null)
+			return;
+		
+		try {
+			writer.close();
+		} catch (IOException e) {}
 	}
 }
