c65755047b336eaf033d7461f8e86f493a0181f7
Niklas Hansen <nikl@itu.dk>
2010-12-06 13:16:25 +0100

    Added method signatures to be implemented in model Reservation

--- Models/Reservation.java
+++ Models/Reservation.java
@@ -1,5 +1,6 @@
 package Models;
 
+import java.sql.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -16,9 +17,28 @@ public class Reservation extends Model {
 	 * is returned on success.
 	 * 
 	 * @param createVars Map containing data to be stored.
+	 * 			key			=> description
+	 * 			customer	=> The ID of the customer to book a car.
+	 * 			carType		=> The ID of the car-type to be booked.
+	 * 			startDate	=> The start date of the booking.
+	 * 			endDate		=> The end date of the booking.
 	 * @return ID on success; -1 on failure.
 	 */
 	public int create (Map<String, Object> createVars) { return -1; }
+	
+	/**
+	 * TODO: Implement this
+	 * Creates an entry in the particular data-source, with 
+	 * the data given in the Map. The ID of the new entry 
+	 * is returned on success.
+	 * 
+	 * @param customer The ID of the customer to book a car.
+	 * @param carType The ID of the car-type to be booked.
+	 * @param startDate The start date of the booking.
+	 * @param endDate The end date of the booking.
+	 * @return ID on success; -1 on failure.
+	 */
+	public int create (int customer, int carType, Date startDate, Date endDate) { return -1; }
 	
 	/**
 	 * TODO: Implement this
@@ -43,9 +63,32 @@ public class Reservation extends Model {
 	 * 
 	 * @param id The ID of the entry to be updated.
 	 * @param updateVars Map containing the data to be updated.
+	 * 			key			=> description
+	 * 			customer	=> The ID of the customer to book a car.
+	 * 			carType		=> The ID of the car-type to be booked.
+	 * 			startType	=> The start date of the booking.
+	 * 			endDate		=> The end date of the booking.
 	 * @return true on success; false on failure.
 	 */
-	public boolean update(int id, Map<String, Object> updateVars) { return false; }
+	public boolean update (int id, Map<String, Object> updateVars) { return false; }
+	
+	/**
+	 * TODO: Implement this
+	 * Updates the entry with the provided ID in the data-
+	 * source. The data to be updated is the keys in the map, 
+	 * and the values are the new data. If then entry is 
+	 * successfully updated, true will be returned. If the 
+	 * update failed (invalid ID or similar), false will 
+	 * be returned.
+	 * 
+	 * @param id The ID of the entry to be updated.
+	 * @param customer The ID of the customer to book a car.
+	 * @param carType The ID of the car-type to be booked.
+	 * @param startDate The start date of the booking.
+	 * @param endDate The end date of the booking.
+	 * @return true on success; false on failure.
+	 */
+	public boolean update (int id, int customer, int carType, Date startDate, Date endDate) { return false; }
 	
 	/**
 	 * TODO: Implement this
