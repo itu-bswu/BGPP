79b2430351f06699014c88073fef871bc8463a35
Ulrik Damm <ulrikdamm@me.com>
2010-12-10 12:13:23 +0100

    tilføjet indre klasser til CustomerOverviewController

--- Controllers/CustomerOverviewController.java
+++ Controllers/CustomerOverviewController.java
@@ -12,52 +12,45 @@ import java.util.Map;
  * Controller - Customer Overview
  *
  */
-public class CustomerOverviewController implements MouseListener, ActionListener {
+public class CustomerOverviewController {
 	CustomerOverview window;
 	List<Map<String, Object>> customers;
 	
 	public CustomerOverviewController(CustomerOverview window) {
 		this.window = window;
 		
-		window.addTableMouseListener(this);
-		window.addSearchListener(this);
+		window.addTableMouseListener(new TableClickListener());
+		window.addSearchListener(new SearchListener());
 		
 		Customer customerModel = new Customer();
 		customers = customerModel.list();
 		window.setValues(customers);
 	}
 	
-	public void actionPerformed(ActionEvent e) {
-		Customer customerModel = new Customer();
-		int phone = 0;
-		if (window.getPhoneString().equals("") == false) {
-			phone = Integer.parseInt(window.getPhoneString());
-		}
-		customers = customerModel.search(window.getNameString(), phone, "name", "ASC");
-		window.setValues(customers);
-	}
-	
-	public void mouseClicked(MouseEvent e) {
-		if (e.getClickCount() == 2) {
-			int row = window.getSelectedRow();
-			int column = window.getSelectedColumn();
-			new EditCustomerController(new EditCustomer((Integer)customers.get(row).get("id")));
+	private class SearchListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			Customer customerModel = new Customer();
+			int phone = 0;
+			if (window.getPhoneString().equals("") == false) {
+				phone = Integer.parseInt(window.getPhoneString());
+			}
+			customers = customerModel.search(window.getNameString(), phone, "name", "ASC");
+			window.setValues(customers);
 		}
 	}
 	
-	public void mouseEntered(MouseEvent e) {
+	private class TableClickListener implements MouseListener {
+		public void mouseClicked(MouseEvent e) {
+			if (e.getClickCount() == 2) {
+				int row = window.getSelectedRow();
+				int column = window.getSelectedColumn();
+				new EditCustomerController(new EditCustomer((Integer)customers.get(row).get("id")));
+			}
+		}
 		
-	}
-	
-	public void mouseExited(MouseEvent e) {
-		
-	}
-	
-	public void mousePressed(MouseEvent e) {
-		
-	}
-	
-	public void mouseReleased(MouseEvent e) {
-		
+		public void mouseEntered(MouseEvent e) {}
+		public void mouseExited(MouseEvent e) {}
+		public void mousePressed(MouseEvent e) {}
+		public void mouseReleased(MouseEvent e) {}
 	}
 }
